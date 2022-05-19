d1d17e53628a9b68a14286ec8cbbbe4a9cab5bc5
xmmword <105472509+xmmword@users.noreply.github.com>
2022-05-19 00:08:17 -0400

    Add files via upload

--- src/zlo.h
+++ src/zlo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 0x80000000
+ * Copyright (C) 2022 xmmword
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
